859c3d9a7369513cc3d86fdee4002ceef4cd63c7
Bogdan <zavodchikov.g08@gmail.com>
2020-02-28 14:00:00 +0300

    fix version

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
-from config_field import __version__
 
 setup(
     name='config_field',
-    version=__version__,
+    version='0.2.8',
     packages=find_packages(),
     author='bzdvdn',
     author_email='bzdv.dn@gmail.com',
     url='https://github.com/bzdvdn/config_field',
-    install_requires=[
+    setup_requires=[
         'Django>=2.2.7',
         'djangorestframework>=3.8.2',
     ]
