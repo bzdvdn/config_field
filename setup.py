10d3ce4b11a31400d2ae68a2f64ae64e658edbfc
bzdvdn <bzdv.dn@gmail.com>
2020-07-25 22:51:49 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='config_field',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     author='bzdvdn',
     author_email='bzdv.dn@gmail.com',
     url='https://github.com/bzdvdn/config_field',
-    setup_requires=[
-        'Django>=2.2.7',
-        'djangorestframework>=3.8.2',
-    ]
+    setup_requires=['Django>=2.2.7', 'djangorestframework>=3.8.2',],
 )
